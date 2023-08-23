4c760e7adf28c17173d608c49df86eaa957676f1
RylemarcAguinaldo <112814126+RylemarcAguinaldo@users.noreply.github.com>
2023-08-23 06:52:30 +0000

    shape

---
+++ Shape.java
@@ -0,0 +1,21 @@
+package shape;
+
+abstract class Shape {
+
+    private double area;
+
+    private double boundingArea;
+
+    void draw() {
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getBoundingArea() {
+        return boundingArea;
+    }
+
+
+}
