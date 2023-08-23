93250ba59e74cade73a0dc990bed8df85786b521
Tom Ellis <s3965617@student.rmit.edu.au>
2023-08-23 16:46:29 +1000

    Staff class

--- src/projects/Person.java
+++ src/projects/Person.java
@@ -1,5 +1,14 @@
 package projects;
 
 public class Person {
-    
+    protected String name;
+    private int id;
+
+    public Person(){
+        
+    }
+
+    public Person(String n, int i, String phone, String e){
+
+    }
 }
