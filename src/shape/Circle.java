700c6e25efdb45237f16b712ff1ae4435dfd356e
Tom Ellis <s3965617@student.rmit.edu.au>
2023-08-23 16:18:51 +1000

    circle

--- src/shape/Circle.java
+++ src/shape/Circle.java
@@ -1,5 +1,19 @@
 package shape;
 
 public class Circle extends Shape {
-    
+    public double getArea(){
+        return 0;
+    }
+
+    public double getBoundingArea(){
+        return 0;
+    }
+
+    public double getRadius(){
+        return 0;
+    }
+
+    public double getPerimeter(){
+        return 0;
+    }
 }
