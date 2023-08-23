66c52bddfcd5c69342ff9d96af61d74fde55b8ec
Ryle <rylea85@gmail.com>
2023-08-23 16:25:06 +1000

    shapes

---
+++ Shape.java
@@ -0,0 +1,19 @@
+public class Shape {
+
+    private double area;
+
+    private double boundingArea;
+
+    void draw() {
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getBoundingArea() {
+        return boundingArea;
+    }
+
+
+}
